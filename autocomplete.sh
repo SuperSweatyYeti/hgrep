dd9186dd8f335bb39b2bcd0dcbc17096d096b6a2
SuperSweatyYeti <supersweatyyeti@gmail.com>
2022-12-01 03:13:07 -0800

    bash completion good progress

--- autocomplete.sh
+++ autocomplete.sh
@@ -1,3 +1,28 @@
+
+_script()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="--help --headers --lines --ignore-case --search --multi-search --file"
+    opts_multi="--multi-search -e"
+    #echo ${COMP_LINE}
+    # Stop showing options after selecting option that requires arguments
+    if [[ ${cur} == -* ]] && [[  ${COMP_LINE} != *--help* && ${COMP_LINE} != *--search* && ${COMP_LINE} != *--multi-search* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    elif [[ ${cur} == -* ]] && [[  ${COMP_LINE} != *--help* && ${COMP_LINE} != *--search* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts_multi}" -- ${cur}) )
+        return 0
+    fi
+
+}
+# Complete options -d for directories -f for files -F then name of function in this script last argument is location of bash script to bash complete for
+complete -d -f -o default -F _script ~/.local/share/bin/hgrep
+
+
+
 # _script()
 # {
 #   _script_commands=$(~/.local/bin/hgrep --complete)
@@ -13,27 +38,3 @@
 # complete -d -f -o default -F _script hgrep
 
 ## bash compltion location: ~/.local/share/bash-completion/completions/hgrep-completion
-
-
-
-
-
-
-
-
-
-_script()
-{
-    local cur prev opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="--help --headers --lines --ignore-case --search --multi-search --file"
-
-    if [[ ${cur} == -* ]] && [[ ! ${prev} == *help* ]] ; then
-        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-        return 0
-    fi
-}
-
-complete -d -f -o default -F _script hgrep
